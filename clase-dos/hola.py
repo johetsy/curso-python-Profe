28394230bc350968eed7a814db4dcff780308918
Richar Lujano <130719479+uruchurtu@users.noreply.github.com>
2023-04-20 19:50:54 -0400

    initial commit

---
+++ clase-dos/hola.py
@@ -0,0 +1,19 @@
+print("Hola Richar!!!!!")
+print("esto es una linea nueva")
+
+edad = 50
+sueldo = 1000.50
+nombre = "Richar"
+isTeacher = True
+
+print(edad, sueldo, nombre, isTeacher)
+
+numero_1 = 1
+numero_2 = 3
+numero_3 = 15
+resultado = ((numero_1 * numero_2) + 100)/numero_3
+print(((numero_1 * numero_2) + 100)/numero_3)
+potencia = 15**3
+sumandos = potencia + numero_3 
+print(sumandos)
+
